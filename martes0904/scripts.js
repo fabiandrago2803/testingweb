ffeb155499f561ee9f992fe56dfb9d73fb7a49b4
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-04-30 22:09:50 -0300

    cambios

--- martes0904/scripts.js
+++ martes0904/scripts.js
@@ -1,9 +1,46 @@
 function validacion()
 {
-    var nombre = document.getElementById("nombre").value;
-    var nombre = document.getElementById("apellido").value;
+    var  nombre = document.getElementById("nombre").value;
+    var  apellido = document.getElementById("apellido").value;
+    const vehiculo = document.querySelectorAll('input[name="vehiculo"]');
+    var email = document.getElementById( "email" ).value;
+    var resultado;
+    console.log("entro");
+    //Validación de que el email este completo
     alert(nombre + " " + apellido);
+   
+    if (nombre == "" || apellido == "" || email =="") {
+      const nombreError = document.getElementById("nombreError");
+      nombreError.removeAttribute("hidden");
+      //alert ("Todos los campos son obligatorios");
+        //return false;
+    } else {
+      //alert('Los datos ingresados son correctos');    
+      //return true;
+    }
+      //console.log(validateEmail("javier@g,ail.com"));
+    if(validateEmail(email))
+        {
+            alert('El mail esta correcto');                
+        }
+        else
+        {
+            alert('el mail no esta correcto');    
+        }
+      if(vehiculo.checked)
+      {
+          alert('Los datos ingresados son correctos');    
+          }
+          else
+          {
+              alert ('Debe seleccionar una opción en el campo Vehículo') ;  
+          }
+     
+   
+      return true;    
 }
+
+function validateEmail(email  )
 
 
 function funcion1() {
@@ -27,3 +64,23 @@ function funcion1() {
   }
   
 
+
+  function funcion1() {
+    //Toma el valor del input con el id="num" y lo guarda en la variable x
+    let x = document.getElementById("num").value;
+    // chequea el valor ingesado
+    let text;
+    if (isNaN(x) || x < 1 || x > 10) // tengo tres condiciones con que una sea verdadera entra al if
+    {     // isNaN(x) pregunta si no es un nro
+      text = "Ingreso no valido";
+    }
+    else
+    {
+      text = "Ingreso valido";
+    }
+  
+    x = x+1
+    document.getElementById("demo").innerHTML = text;
+    document.getElementById("numero").innerHTML = x;
+  }
+  
